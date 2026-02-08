63644a9541208cfabe524ff968eab9578fa0555d
greg <greg.micah.jensen@gmail.com>
2026-02-08 00:01:00 -0500

    converts upperbound constraints to equality constraints

--- src/linear_optimization_problem.rs
+++ src/linear_optimization_problem.rs
@@ -8,26 +8,80 @@ pub struct UpperBoundConstraint {
 }
 
 impl UpperBoundConstraint {
-    pub fn new(coefficients: Coefficients, constraint: Value) -> Self {
-        Self {coefficients: coefficients, constraint: constraint}
+    pub fn new(coefficients: &Coefficients, constraint: Value) -> Self {
+        Self {coefficients: coefficients.clone(), constraint: constraint}
     }
 }
-
-type Variable = usize;
 
 pub fn solve_standard_problem(
     objective_function: &Coefficients, 
     functional_constraints: &Vec<UpperBoundConstraint>) -> Vec<Value> {
-        let mut functional_constraints = functional_constraints.clone();
+        let mut equality_constraints = equality_constraints(functional_constraints);
         let variable_count = objective_function.len();
         let mut solns = vec![0_f32; variable_count];
         for var in 0..variable_count {
-            solns[var] = next_maximal_val(var,&mut functional_constraints);
+            solns[var] = next_maximal_val(var,&mut equality_constraints);
         }
         solns
 }
 
-fn next_maximal_val(variable: Variable, constraints: &mut Vec<UpperBoundConstraint>) -> Value {
+type Variable = usize;
+
+#[derive(PartialEq)]
+#[derive(Debug)]
+struct Equation {
+    coefficients: Coefficients,
+    constraint: Value
+}
+
+struct OptimizationProblem {
+    objective_function: Coefficients,
+    functional_constraints: Vec<Equation>,
+    basic_variables: Vec<Variable>
+}
+
+impl OptimizationProblem {
+    pub fn new(objective_function: &Coefficients, functional_constraints: &Vec<UpperBoundConstraint>) -> Self {
+        Self {
+            objective_function: objective_function.clone(),
+            functional_constraints: equality_constraints(functional_constraints),
+            basic_variables: Vec::new()
+        }
+    }
+}
+
+fn equality_constraints(functional_constraints: &Vec<UpperBoundConstraint>) -> Vec<Equation> {
+    let count = functional_constraints.len();
+    functional_constraints
+        .iter().enumerate()
+        .map(|(idx, constraint)| equality_constraint(idx, constraint, count))
+        .collect()
+}
+
+fn equality_constraint(
+    constraint_index: usize,
+    bound_constraint: &UpperBoundConstraint, 
+    constraint_count: usize) -> Equation {
+        let coeffs = &bound_constraint.coefficients;
+        let coeffs = with_slack_variable(constraint_index, coeffs, constraint_count);
+        Equation{
+            coefficients: coeffs,
+            constraint: bound_constraint.constraint
+        }
+}
+
+fn with_slack_variable(
+    constraint_index: usize, 
+    coefficients: &Vec<Value>,
+    constraint_count: usize) -> Vec<Value> {
+        let mut coeffs = coefficients.clone();
+        for i in 0..constraint_count {
+            coeffs.push(if i == constraint_index {1_f32} else {0_f32});
+        }
+        coeffs
+}
+
+fn next_maximal_val(variable: Variable, constraints: &mut Vec<Equation>) -> Value {
     constraints.iter_mut()
         .filter(|constraint| constraint.coefficients[variable] != 0_f32)
         .map(|constraint| maximal_val(variable,constraint))
@@ -35,7 +89,7 @@ fn next_maximal_val(variable: Variable, constraints: &mut Vec<UpperBoundConstrai
         .unwrap_or(0_f32)
 }
 
-fn maximal_val(variable: Variable, constraint: &mut UpperBoundConstraint) -> Value {
+fn maximal_val(variable: Variable, constraint: &mut Equation) -> Value {
     let upper_bound = constraint.constraint;
     let coeff = constraint.coefficients[variable];
     constraint.constraint -= upper_bound; // eventually remove i think
--- src/linear_optimization_problem/test.rs
+++ src/linear_optimization_problem/test.rs
@@ -1,7 +1,7 @@
 
 #[cfg(test)]
 mod tests {
-    use crate::linear_optimization_problem::{self as sut, test};
+    use crate::linear_optimization_problem::{self as sut};
 
     #[test]
     fn solves_one_variable_zero_constraint_problem() {
@@ -24,7 +24,7 @@ mod tests {
     #[test]
     fn solves_one_variable_one_constraint_problem() {
         let objective_function = vec![1_f32];
-        let functional_constraint = sut::UpperBoundConstraint::new(vec![1_f32],1_f32);
+        let functional_constraint = sut::UpperBoundConstraint::new(&vec![1_f32],1_f32);
         let fn_constraints = vec![functional_constraint];
         let solns = sut::solve_standard_problem(&objective_function,&fn_constraints);
         let expected_solns = vec![1_f32];
@@ -34,7 +34,7 @@ mod tests {
     #[test]
     fn maximizes_unconstrainted_variable() {
         let objective_function = vec![1_f32, 1_f32];
-        let functional_constraint = sut::UpperBoundConstraint::new(vec![3_f32, 0_f32],6_f32);
+        let functional_constraint = sut::UpperBoundConstraint::new(&vec![3_f32, 0_f32],6_f32);
         let fn_constraints = vec![functional_constraint];
         let solns = sut::solve_standard_problem(&objective_function,&fn_constraints);
         let expected_solns = vec![2_f32, 0_f32];
@@ -44,7 +44,7 @@ mod tests {
     #[test]
     fn maximizes_one_variable_problem() {
         let objective_function = vec![1_f32];
-        let functional_constraint = sut::UpperBoundConstraint::new(vec![3_f32],6_f32);
+        let functional_constraint = sut::UpperBoundConstraint::new(&vec![3_f32],6_f32);
         let fn_constraints = vec![functional_constraint];
         let solns = sut::solve_standard_problem(&objective_function,&fn_constraints);
         let expected_solns = vec![2_f32];
@@ -54,8 +54,8 @@ mod tests {
     #[test]
     fn maximizes_one_variable_two_constraint_problem() {
         let objective_function = vec![1_f32];
-        let fn_constaint_0 = sut::UpperBoundConstraint::new(vec![2_f32],6_f32);
-        let fn_constaint_1 = sut::UpperBoundConstraint::new(vec![3_f32], 6_f32);
+        let fn_constaint_0 = sut::UpperBoundConstraint::new(&vec![2_f32],6_f32);
+        let fn_constaint_1 = sut::UpperBoundConstraint::new(&vec![3_f32], 6_f32);
         let fn_constraints = vec![fn_constaint_0, fn_constaint_1];
         let solns = sut::solve_standard_problem(&objective_function,&fn_constraints);
         let expected_solns = vec![2_f32];
@@ -65,8 +65,8 @@ mod tests {
     #[test]
     fn maximizes_two_independent_variable_two_constraint_problem() {
         let objective_function = vec![1_f32, 1_f32];
-        let fn_constaint_0 = sut::UpperBoundConstraint::new(vec![2_f32,0_f32],6_f32);
-        let fn_constaint_1 = sut::UpperBoundConstraint::new(vec![0_f32,4_f32],8_f32);
+        let fn_constaint_0 = sut::UpperBoundConstraint::new(&vec![2_f32,0_f32],6_f32);
+        let fn_constaint_1 = sut::UpperBoundConstraint::new(&vec![0_f32,4_f32],8_f32);
         let fn_constraints = vec![fn_constaint_0, fn_constaint_1];
         let solns = sut::solve_standard_problem(&objective_function,&fn_constraints);
         let expected_solns = vec![3_f32,2_f32];
@@ -76,10 +76,96 @@ mod tests {
     #[test]
     fn maximizes_two_dependent_variable_one_constraint_symmetric_problem() {
         let objective_function = vec![1_f32, 1_f32];
-        let fn_constaint_0 = sut::UpperBoundConstraint::new(vec![2_f32,4_f32],6_f32);
+        let fn_constaint_0 = sut::UpperBoundConstraint::new(&vec![2_f32,4_f32],6_f32);
         let fn_constraints = vec![fn_constaint_0];
         let solns = sut::solve_standard_problem(&objective_function,&fn_constraints);
         let expected_solns = vec![3_f32,0_f32];
         assert_eq!(expected_solns, solns);
+    }
+
+    // #[test]
+    // fn maximizes_two_dependent_variable_two_constraint_symmetric_problem() {
+    //     let objective_function = vec![1_f32, 1_f32];
+    //     let fn_constaint_0 = sut::UpperBoundConstraint::new(&vec![1_f32,2_f32],3_f32);
+    //     let fn_constaint_1 = sut::UpperBoundConstraint::new(&vec![2_f32,1_f32],3_f32);
+    //     let fn_constraints = vec![fn_constaint_0,fn_constaint_1];
+    //     let solns = sut::solve_standard_problem(&objective_function,&fn_constraints);
+    //     let expected_solns = vec![2_f32,2_f32];
+    //     assert_eq!(expected_solns, solns);
+    // }
+
+    mod equality_constraints {
+        use super::*;
+
+        #[test]
+        fn converts_zero_constraints() {
+            let ub_constraints = Vec::<sut::UpperBoundConstraint>::new();
+            let eq_constraints = sut::equality_constraints(&ub_constraints);
+            let expected_constraints = Vec::<sut::Equation>::new();
+            assert_eq!(expected_constraints,eq_constraints);
+        }
+
+        #[test]
+        fn converts_one_constraint_one_variable() {
+            let ub_constraints = vec![
+                sut::UpperBoundConstraint {
+                    coefficients: vec![1_f32],
+                    constraint: 5_f32
+                }
+            ];
+            let eq_constraints = sut::equality_constraints(&ub_constraints);
+            let expected_constraints = vec![
+                sut::Equation {
+                    coefficients: vec![1_f32, 1_f32],
+                    constraint: 5_f32
+                }
+            ];
+            assert_eq!(expected_constraints,eq_constraints);
+        }
+
+        #[test]
+        fn converts_one_constraint_two_variables() {
+            let ub_constraints = vec![
+                sut::UpperBoundConstraint {
+                    coefficients: vec![1_f32, 2_f32],
+                    constraint: 5_f32
+                }
+            ];
+            let eq_constraints = sut::equality_constraints(&ub_constraints);
+            let expected_constraints = vec![
+                sut::Equation {
+                    coefficients: vec![1_f32, 2_f32, 1_f32],
+                    constraint: 5_f32
+                }
+            ];
+            assert_eq!(expected_constraints,eq_constraints);
+        }
+
+        #[test]
+        fn converts_two_constraints_two_variables() {
+            let ub_constraints = vec![
+                sut::UpperBoundConstraint{
+                    coefficients: vec![1_f32, 2_f32],
+                    constraint: 5_f32
+                },
+                sut::UpperBoundConstraint{
+                    coefficients: vec![3_f32, 4_f32],
+                    constraint: 10_f32
+                }
+            ];
+            let eq_constraints = sut::equality_constraints(&ub_constraints);
+            let expected_constraints = vec![
+                sut::Equation {
+                    coefficients: vec![1_f32, 2_f32, 1_f32, 0_f32],
+                    constraint: 5_f32
+                },
+                sut::Equation {
+                    coefficients: vec![3_f32, 4_f32, 0_f32, 1_f32],
+                    constraint: 10_f32
+                }
+            ];
+            assert_eq!(expected_constraints,eq_constraints);
+        }
+
     }
 }
